e2f2cdfa44221bf0efec138387723bbebb53b181
Evgeny Simonov <jdsimonov2@gmail.com>
2025-08-20 15:33:20 +0300

    modified:   src/components/article-params-form/ArticleParamsForm.tsx

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,17 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false)
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={() => {
+				setIsOpen(!isOpen);
+			}} />
+			<aside className={`${styles.container} ${isOpen ? styles.container_open : ''}`}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
