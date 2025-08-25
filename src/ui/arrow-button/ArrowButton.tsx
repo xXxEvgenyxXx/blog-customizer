02a5e3f78bb9c1f227faa4f61caece174949e0db
Evgeny Simonov <jdsimonov2@gmail.com>
2025-08-25 08:04:14 +0300

    Revert "	modified:   src/components/article-params-form/ArticleParamsForm.tsx"
    This reverts commit 7e13743ebf541cb99fe2fa488c839a51623276a9.

--- src/ui/arrow-button/ArrowButton.tsx
+++ src/ui/arrow-button/ArrowButton.tsx
@@ -1,8 +1,9 @@
-import { forwardRef } from 'react';
 import arrow from 'src/images/arrow.svg';
+
 import styles from './ArrowButton.module.scss';
 import clsx from 'clsx';
 
+/** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
 type ArrowButtonProps = {
@@ -10,22 +11,20 @@ type ArrowButtonProps = {
 	onClick: OnClick;
 };
 
-export const ArrowButton = forwardRef<HTMLDivElement, ArrowButtonProps>(
-	({ isOpen, onClick }, ref) => {
-		return (
-			<div
-				ref={ref}
-				role='button'
-				aria-label='Открыть/Закрыть форму параметров статьи'
-				tabIndex={0}
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}
-				onClick={onClick}>
-				<img
-					src={arrow}
-					alt='иконка стрелочки'
-					className={clsx(styles.arrow, { [styles.arrow_open]: isOpen })}
-				/>
-			</div>
-		);
-	}
-);
+export const ArrowButton = ({ isOpen, onClick }: ArrowButtonProps) => {
+	return (
+		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
+		<div
+			role='button'
+			aria-label='Открыть/Закрыть форму параметров статьи'
+			tabIndex={0}
+			className={clsx(styles.container, { [styles.container_open]: isOpen })}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, { [styles.arrow_open]: isOpen })}
+			/>
+		</div>
+	);
+};
